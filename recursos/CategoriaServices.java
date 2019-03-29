782b426beb38a9d5cfa2f2bd2942d1df29168d75
Naren David <narenkun96@gmail.com>
2019-03-29 11:51:36 -0500

    herramientas

---
+++ recursos/CategoriaServices.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.services;
+
+import com.dao.ImplDao;
+import com.entity.Categoria;
+import com.implDao.ICategoria;
+import java.io.Serializable;
+/**
+ *
+ * @author Jcmm
+ */
+public class CategoriaServices extends ImplDao<Categoria, Long> implements ICategoria,Serializable{
+   
+}
